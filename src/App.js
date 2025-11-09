59a791499e0f103f936cdaa3f6445230fc85c6c2
tauhidur311-star <tauhidur311@gmail.com>
2025-11-10 02:39:56 +0600

    Fix: Remove unused imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Store from './pages/store/Store';
 import AdminDashboard from './pages/admin/AdminDashboard';
