6b5e305ca99a9b908aadec64d4f0ffcbd07e684a
tauhidur311-star <tauhidur311@gmail.com>
2025-11-10 04:25:41 +0600

    Fix: Import Navigate component in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Store from './pages/store/Store';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import AuthPage from './pages/auth/AuthPage';
