f80e4b012668d174db3bb381229adf733189aeed
tauhidur311-star <tauhidur311@gmail.com>
2025-11-10 18:57:27 +0600

    Style: Tweak default parameters for Silk background

--- src/components/Silk.js
+++ src/components/Silk.js
@@ -91,7 +91,7 @@ const SilkPlane = forwardRef(function SilkPlane({ uniforms }, ref) {
 });
 SilkPlane.displayName = 'SilkPlane';
 
-const Silk = ({ speed = 5, scale = 1, color = '#7B7481', noiseIntensity = 1.5, rotation = 0 }) => {
+const Silk = ({ speed = 6.5, scale = 1, color = '#3612c0', noiseIntensity = 1, rotation = 0.76 }) => {
   const meshRef = useRef();
 
   const uniforms = useMemo(
